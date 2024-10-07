da3e4cdd5606b48e5da3bbf18cdf6f12ab446efe
Do-oya <qkrwjdgus1441@naver.com>
2024-10-07 21:48:32 +0900

    [김영한 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발]

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",                   // 중간 테이블 이름
+            joinColumns = @JoinColumn(name = "category_id"),  // Category 테이블의 외래 키
+            inverseJoinColumns = @JoinColumn(name = "item_id")  // Item 테이블의 외래 키
+    )
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+
+}
